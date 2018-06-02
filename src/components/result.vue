c6428cef8b1fa4befa71eda51a47846707a7131e
hernani <hernani.luiz@gmail.com>
2018-06-02 17:54:00 -0300

    Result component

---
+++ src/components/result.vue
@@ -0,0 +1,26 @@
+<template>
+	<section class="result">
+		<ul>
+			<li class="result-item" v-for="(result, index) in resultSearch">
+				<details>
+					<summary><h2> {{ result.name }}</h2></summary>
+					<div class="result-item--infos">
+						<p>Gender: {{result.gender}}</p>
+					</div>
+				</details>
+			</li>
+		</ul>
+	</section>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+  computed: {
+    resultSearch() {
+      return this.$store.state.searchResult.results;
+    },
+  },
+};
+</script>
+
