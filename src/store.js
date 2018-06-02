ab2adbfc678e81abf791af00a7056c92e46ba601
hernani <hernani.luiz@gmail.com>
2018-06-02 17:50:59 -0300

    Store Search Action

---
+++ src/store.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { HTTP  } from './stores/config/http-common';
+
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    searchResult: {},
+    errors: [],
+  },
+  mutations: {
+    GET_RESULT_SEARCH: (state, payload) => {
+      state.searchResult = payload;
+    },
+    GET_ERRORS: (state, payload) => {
+      state.searchResult = payload;
+    },
+  },
+  actions: {
+    SEARCH: ({
+      commit,
+    }, payload) => new Promise((resolve, reject) => {
+      HTTP.get(`?search=${payload}`).then((response) => {
+        resolve(response);
+        commit('GET_RESULT_SEARCH', response.data);
+      }).catch((erro) => {
+        reject(erro);
+        commit('GET_ERRORS', erro);
+      });
+    }),
+  },
+});
