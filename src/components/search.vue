0ff4093eda10266ac06a2288ff384d31787b6c67
hernani <hernani.luiz@gmail.com>
2018-06-02 17:53:49 -0300

    Search component

---
+++ src/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+	<main>
+		<form @submit.prevent="searchTerms" class="form-search" :class="{'top':(resultSearchCount > 0)}">
+			<input type="text" name="" id="" placeholder="Ex: r2" v-model="search">
+			<input type="submit" value="Buscar">
+		</form>
+		<span class="result-total" v-if="this.resultSearchCount > 0">Total: {{this.resultSearchCount}}</span>
+	</main>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+	  return {
+		  search: '',
+	  };
+  },
+  computed: {
+    resultSearchCount() {
+      return this.$store.state.searchResult.count;
+    },
+  },
+  methods: {
+    searchTerms() {
+	  this.$root.$emit('search', this.search);
+	  this.$store.dispatch('SEARCH', this.search);
+	  this.search = '';
+    },
+  },
+};
+</script>
+
