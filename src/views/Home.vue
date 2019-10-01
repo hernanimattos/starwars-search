7129ef8c621d9a987554a750f5b9211156ae5643
Hernani Mattos <hernanimattos@Hernanis-MacBook-Pro.local>
2019-10-01 13:55:43 -0300

    teste build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,23 @@
 <template>
-	<main>
-		<search/>
-		<result/>
-		<paginate/>
-	</main>
+  <main>
+    <p>Aquii</p>
+    <search />
+    <result />
+    <paginate />
+  </main>
 </template>
 
 <script>
-import search from '@/components/search.vue';
-import result from '@/components/result.vue';
-import paginate from '@/components/paginate.vue';
+import search from "@/components/search.vue";
+import result from "@/components/result.vue";
+import paginate from "@/components/paginate.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-	  search,
-	  result,
-	  paginate,
+    search,
+    result,
+    paginate
   },
   computed: {
     resultSearchCount() {
@@ -27,31 +28,31 @@ export default {
     },
     result() {
       return this.$store.state.searchResult;
-    },
+    }
   },
   methods: {
     searchTerms() {
-	  this.$store.dispatch('SEARCH', this.search);
-	  this.search = '';
+      this.$store.dispatch("SEARCH", this.search);
+      this.search = "";
     },
     seachParamUrl(url, term) {
-      const query = url.split('?')[1].split('&');
-	  const valueQuery = query.map((q) => {
-        const queryArray = q.split('=');
+      const query = url.split("?")[1].split("&");
+      const valueQuery = query.map(q => {
+        const queryArray = q.split("=");
         if (queryArray.indexOf(term) === 0) {
           return queryArray[1] || [];
         }
-	  });
+      });
       return valueQuery[0];
     },
     changePage(url, term) {
       const searchParam = {
-      	  page: this.seachParamUrl(url, term),
-      	  term: this.search,
+        page: this.seachParamUrl(url, term),
+        term: this.search
       };
-      this.$store.dispatch('CHANGE_PAGE', searchParam);
-    },
-  },
+      this.$store.dispatch("CHANGE_PAGE", searchParam);
+    }
+  }
 };
 </script>
 
