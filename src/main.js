b877bbede7f952f013769899d1dbbaf1ab8b4524
Hernani Mattos <hernanimattos@Hernanis-MacBook-Pro.local>
2019-10-01 14:25:55 -0300

    Config gtm

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import VueTagManager from 'vue-tag-manager';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -8,6 +9,10 @@ import './assets/styles/reset.css';
 Vue.filter('formatHeight', value => value.replace(/([0-9]{2})$/g, ',$1'));
 
 Vue.config.productionTip = false;
+
+Vue.use(VueTagManager, {
+  gtmId: 'GTM-P2S3FSV',
+});
 
 new Vue({
   store,
